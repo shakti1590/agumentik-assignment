3dce973a5e301cc1acf58ffa0ca1cd131d7bd865
shakti singh <60788912+shakti1590@users.noreply.github.com>
2023-04-20 03:24:56 +0530

    Delete backend directory

--- backend/src/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connect = async ()=>{
-    return await mongoose.connect("mongodb+srv://agumentik:agumentik@cluster0.yukkdpk.mongodb.net/test");
-    
-}
-
-module.exports = connect;
